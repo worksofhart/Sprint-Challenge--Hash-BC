4f80b81d443045847a1ce28933483a6cea62784e
Michael Hart <michael@mhart.dev>
2020-01-17 13:29:55 -0500

    Cleared flake8 warnings

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
@@ -15,7 +13,7 @@ def get_indices_of_item_weights(weights, length, limit):
             return (max(index_1, index_2), min(index_1, index_2))
         else:
             hash_table_insert(ht, weight, index_1)
-        
+
     return None
 
 
